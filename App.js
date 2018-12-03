2ab137a2eca8d8c474dadbcda124ed1589a18fe9
Jazzonz <jasonwong640@gmail.com>
2018-12-03 22:00:18 +0800

    initial commit

---
+++ App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>HAHA Dominic is stuck!</Text>
+         <Text> See, never listen to instructions!</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+    alignItems: 'center',
+    justifyContent: 'center',
+  }
+});
